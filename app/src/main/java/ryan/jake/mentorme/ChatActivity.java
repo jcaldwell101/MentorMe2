f51257044625d7273f842afab68883c50fba26e3
jd <rootjd@gmail.com>
2016-11-30 18:49:42 -0500

    chatactivity dependencies

--- app/src/main/java/ryan/jake/mentorme/ChatActivity.java
+++ app/src/main/java/ryan/jake/mentorme/ChatActivity.java
@@ -1,6 +1,134 @@
 package ryan.jake.mentorme;
 
-import android.os.Bundle;
+
+        import android.app.Activity;
+        import android.database.DataSetObserver;
+        import android.net.Uri;
+        import android.os.Bundle;
+        import android.support.design.widget.FloatingActionButton;
+        import android.view.KeyEvent;
+        import android.view.View;
+        import android.widget.AbsListView;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.ListView;
+
+        import com.google.android.gms.appindexing.Action;
+        import com.google.android.gms.appindexing.AppIndex;
+        import com.google.android.gms.appindexing.Thing;
+        import com.google.android.gms.common.api.GoogleApiClient;
+
+
+public class ChatActivity extends Activity {
+    private static final String TAG = "ChatActivity";
+
+    private ChatArrayAdapter chatArrayAdapter;
+    private ListView listView;
+    private EditText chatText;
+    private FloatingActionButton buttonSend;
+    private boolean side = false;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton buttonSend = (FloatingActionButton) findViewById(R.id.fab);
+
+        listView = (ListView) findViewById(R.id.msgview);
+
+        chatArrayAdapter = new ChatArrayAdapter(getApplicationContext(), R.layout.right);
+        listView.setAdapter(chatArrayAdapter);
+
+        chatText = (EditText) findViewById(R.id.msg);
+        chatText.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    return sendChatMessage();
+                }
+                return false;
+            }
+        });
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                sendChatMessage();
+            }
+        });
+
+        listView.setTranscriptMode(AbsListView.TRANSCRIPT_MODE_ALWAYS_SCROLL);
+        listView.setAdapter(chatArrayAdapter);
+
+        //to scroll the list view to bottom on data change
+        chatArrayAdapter.registerDataSetObserver(new DataSetObserver() {
+            @Override
+            public void onChanged() {
+                super.onChanged();
+                listView.setSelection(chatArrayAdapter.getCount() - 1);
+            }
+        });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    private boolean sendChatMessage() {
+        chatArrayAdapter.add(new ChatMessage(side, chatText.getText().toString()));
+        chatText.setText("");
+        side = !side;
+        return true;
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Chat Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+}
+
+
+
+
+
+
+
+/*import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -27,3 +155,4 @@ public class ChatActivity extends AppCompatActivity {
     }
 
 }
+*/
---
+++ app/src/main/java/ryan/jake/mentorme/ChatMessage.java
@@ -0,0 +1,16 @@
+package ryan.jake.mentorme;
+
+/**
+ * Created by jd on 11/30/2016.
+ */
+
+public class ChatMessage {
+    public boolean left;
+    public String message;
+
+    public ChatMessage(boolean left, String message) {
+        super();
+        this.left = left;
+        this.message = message;
+    }
+}
