cdb9fbabbae52b9c32336d124c0992080cf350ae
jcaldwell101 <jakecaldwell101@yahoo.com>
2016-12-01 20:19:58 -0500

    results activity missing

--- app/src/main/java/ryan/jake/mentorme/resultsActivity.java
+++ app/src/main/java/ryan/jake/mentorme/resultsActivity.java
@@ -1,44 +1,112 @@
 package ryan.jake.mentorme;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class ResultsActivity extends AppCompatActivity {
+    public static final String TAG = ResultsActivity.class.getSimpleName();
 
+    Handler mHandler;
+    private JSONObject mainJSONObject;
+    private JSONArray jsonArr;
 
-    ArrayList<String> animalsNameList;
-    private ListView mAnimals;
+    private ListView mNames;
+
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
 
+        mHandler = new Handler(Looper.getMainLooper());
 
+        mNames = (ListView)findViewById(R.id.resultsView);
 
+        Log.v(TAG, "searchcheck");
+
+        getNames();
 
 
     }
 
-    void getAnimalNames()
-    {
-        animalsNameList.add("DOG");
-        animalsNameList.add("CAT");
-        animalsNameList.add("HORSE");
-        animalsNameList.add("ELEPHANT");
-        animalsNameList.add("LION");
-        animalsNameList.add("COW");
-        animalsNameList.add("MONKEY");
-        animalsNameList.add("DEER");
-        animalsNameList.add("RABBIT");
-        animalsNameList.add("BEER");
-        animalsNameList.add("DONKEY");
-        animalsNameList.add("LAMB");
-        animalsNameList.add("GOAT");
 
+
+
+    private void getNames(){
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("http://ec2-54-218-89-13.us-west-2.compute.amazonaws.com/search")
+                .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e(TAG, "Fail", e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    if (response.isSuccessful()){
+
+                         //response here
+
+                        String jsonData = response.body().string();
+                        JSONObject jsonObj = null;
+
+                        jsonObj = new JSONObject(jsonData);
+                        jsonArr = jsonObj.getJSONArray("username");
+
+
+
+                        response.close();
+
+                        mHandler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                Log.v(TAG, "check");
+
+                            }
+                        });
+
+                    }else{
+                        mHandler.post(new Runnable() {
+                            @Override
+                            public void run() {
+
+                            }
+                        });
+
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG,"Exception Caught",e);
+                } catch (JSONException e) {
+
+                    e.printStackTrace();
+                }
+            }
+        });
 
     }
 }
